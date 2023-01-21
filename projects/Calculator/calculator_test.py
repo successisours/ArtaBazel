10a3cd2b5a54753f9c5de4aa711bc8d804f00b1e
Karthikeyan Rajendran <k@k-7.org>
2023-01-21 12:13:36 -0800

    Adding new test

--- projects/Calculator/calculator_test.py
+++ projects/Calculator/calculator_test.py
@@ -4,8 +4,11 @@ from projects.Calculator.calculator import Calculator
 class TestSum(unittest.TestCase):
     def test_sum(self):
         calculator = Calculator()
-        self.assertEqual(calculator.add(1,2), 3) 
-
+        self.assertEqual(calculator.add(1,2), 5) 
+    
+    def test_large_sum(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(1000, 2000), 3000)
 
 if __name__ == '__main__':
     unittest.main()
